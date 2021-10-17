ad31ce5998aeb2ef0ad75b9fd8ef41dbf40a00ae
leovenom <leonardt@outlook.com>
2021-10-17 18:54:51 +0200

    adding feature

--- src/components/header/mobile-drawer.js
+++ src/components/header/mobile-drawer.js
@@ -44,7 +44,34 @@ export default function MobileDrawer() {
      closeBtnStyle={styles.close}
    >
      <Scrollbars autoHide>
-
+      <Box sx={styles.content}>
+        <Box sx={styles.menu}>
+          {menuItems.map((menuItem, i) => (
+            <Link
+              activeClass="active"
+              to={menuItem.path}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              key={i}
+            >
+              {menuItem.label}
+            </Link>
+          ))}
+        </Box>
+        <Box sx={styles.menuFooter}>
+          <Box sx={styles.social}>
+            {social.map((socialItem, i) => (
+              <Box as="span" key={i} sx={styles.social.icon}>
+                <Link to={socialItem.path}>
+                  {socialItem.icon}
+                </Link>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Box>
      </Scrollbars>
    </Drawer>
   );
